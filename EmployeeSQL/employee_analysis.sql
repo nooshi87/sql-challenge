7f8a9aab699be30a1a691e36151cfe896b0d2a9c
Anushya <nooshi87@gmail.com>
2023-11-08 15:19:32 -0800

    re-saved data analysis for challenge 9 as it did not push before

--- EmployeeSQL/employee_analysis.sql
+++ EmployeeSQL/employee_analysis.sql
@@ -1,7 +1,6 @@
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-
 CREATE TABLE "titles" (
     "title_id" VARCHAR   NOT NULL,
     "title" VARCHAR   NOT NULL,
@@ -73,4 +72,80 @@ REFERENCES "departments" ("dept_no");
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
-SELECT * FROM employees
+-- Create a view with only information of employees requested for part 1
+DROP VIEW  IF EXISTS employees_salary;
+
+CREATE VIEW  employees_salary AS
+select employees.*, salaries.salary
+from employees
+left join salaries on salaries.emp_no = employees.emp_no;
+-- Data analysis Part 1
+SELECT * FROM employees_salary;
+SELECT emp_no, last_name, first_name, sex, salary from employees_salary
+ORDER BY emp_no;
+
+-- Create sub-query to selecting appropriate information for Data analysis Part 2 (hired only after 1986):
+SELECT first_name, last_name, hire_date 
+FROM employees 
+WHERE (hire_date >= CAST ('1986-01-01' AS DATE))
+ORDER BY hire_date;
+
+--Data analysis part 3
+--Create a view where department numbers are added to employee table
+DROP VIEW  IF EXISTS employee_departments;
+
+CREATE VIEW employee_departments AS
+select employees.*, dept_emp.dept_no
+from employees
+left join dept_emp on dept_emp.emp_no = employees.emp_no;
+
+--Add department name to employees_departments view
+CREATE VIEW dept_manager_info AS
+SELECT employee_departments.*, departments.dept_name
+FROM employee_departments
+JOIN departments 
+  ON departments.dept_no = employee_departments.dept_no;
+  
+-- Join dept_manager table
+DROP VIEW IF EXISTS dept_managers_deets; 
+Create view dept_managers_deets AS
+SELECT dept_manager_info.* from dept_manager_info
+JOIN dept_manager ON dept_manager.emp_no = dept_manager_info.emp_no;
+
+--select columns requested for data analysis part 3
+Select dept_no,dept_name,emp_no, last_name, first_name FROM dept_managers_deets;
+
+--Data analysis part 4
+--Use view created above called 'dept_manager_info' to get department name and dept_no for every employee
+SELECT * FROM dept_manager_info;
+--Select columns requested for part 4 using view
+SELECT dept_no, emp_no, last_name, first_name, dept_name FROM dept_manager_info;
+
+--Data analysis part 5: List of employees with first name = Hercules & last name starts with B
+SELECT * FROM employees;
+SELECT first_name, last_name, sex 
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+--Data analysis part 6
+--Use view created above called 'dept_manager_info' 
+--This view has the department no and dept name for every employee. Get list of employees in 'Sales'
+SELECT * FROM dept_manager_info;
+SELECT dept_name, emp_no, last_name, first_name 
+FROM dept_manager_info
+WHERE dept_name = 'Sales';
+
+--Data analysis part 7
+--Use view created above called 'dept_manager_info' 
+--This view has the department no and dept name for every employee . Get list of employess in 'Sales' or 'Development'
+SELECT * FROM dept_manager_info;
+SELECT dept_name, emp_no, last_name, first_name 
+FROM dept_manager_info
+WHERE dept_name = 'Sales' OR dept_name = 'Development';
+
+--Data analysis part 8
+--Use 'employees' table since only last name per employee info is required
+SELECT last_name, COUNT(last_name) AS "last_name_ct"
+FROM employees
+GROUP BY last_name
+ORDER BY "last_name_ct" DESC;
